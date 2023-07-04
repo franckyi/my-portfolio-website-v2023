981aca6612f84285f5a7a9b25166b8edc4d06d78
Francky Iside <francky@tuta.io>
2023-07-04 20:39:21 +0200

    setup angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { ProjectComponent } from './components/project/project.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+// import {MatDividerModule} from '@angular/material/divider';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatSelectModule} from '@angular/material/select';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatTableModule} from '@angular/material/table';
+// import {MatPaginatorModule} from '@angular/material/paginator';
+// import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    // MatButtonModule,
+    // MatTabsModule,
+    // MatDividerModule,
+    // MatFormFieldModule,
+    // MatSelectModule,
+    // MatInputModule,
+    // MatTableModule,
+    // MatPaginatorModule,
+    // MatListModule
+  ]
+})
+export class MaterialModule { }
